bf194434c04b2a6eab390d45859e6b3239a6a643
paulsuman <paulsuman7861@gmail.com>
2017-01-05 12:49:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone} from '@angular/core';
+import {FacebookService} from './facebook.service';
+
+declare const FB:any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    providers: [FacebookService]
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent implements OnInit{
+	name="";
+	isUser = false;
+
+  	constructor(
+  		private _ngZone: NgZone,
+		private _facebookService: FacebookService
+  	){}
+
+	ngOnInit(){
+    		this._facebookService.loadAndInitFBSDK();
+	}
+
+	login(){
+		var self = this;
+		FB.login(function(response)
+		 {
+		    if (response.authResponse)
+		     {
+		        FB.api('/me',{fields:'name'}, function(response)
+		         {
+		          	self._ngZone.run(() => {
+				        // self.name = response.name;
+				        // self.isUser = true;
+				        console.log(response.name);
+			        });
+
+		        });
+		    }
+		    else{
+		        console.log('User cancelled login or did not fully authorize.');
+		    }
+		});
+}}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
+@NgModule({ 
   declarations: [
     AppComponent
   ],
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
